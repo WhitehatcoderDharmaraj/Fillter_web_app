3b102a2a12150f9ede7d03b54e80999829017969
Dharmaraj <90249634+WhitehatcoderDharmaraj@users.noreply.github.com>
2022-05-16 17:38:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ nose_x=0;
 nose_y=0;
 
 function preload(){
-clown_nose=loadImage('https://i.postimg.cc/YSY5GF7F/download.p');
+mustache=loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
 function setup(){
-canvas=createCanvas(300,300);
+canvas=createCanvas(300,300)    ;
 canvas.center();
 
 video=createCapture(VIDEO);
@@ -21,9 +21,9 @@ function gotPoses(results){
     if(results.length>0){
         console.log(results);
 
-nose_x=results[0].pose.nose.x-10;
-nose_y=results[0].pose.nose.y-5;
-console.log("nose x = "+nose_x+" and nose y = "+nose_y)
+        nose_x=results[0].pose.nose.x-20;
+        nose_y=results[0].pose.nose.y;
+        console.log("nose x = "+nose_x+" and nose y = "+nose_y)
     }
 }
 
@@ -33,10 +33,7 @@ function modelLoaded(){
 
 function draw(){
 image(video,0,0,300,300);
-/*fill("red");
-stroke("red");
-circle(nose_x,nose_y,20);*/
-image(clown_nose,nose_x,nose_y,20,20);
+image(mustache,nose_x,nose_y,50,35);
 }
 
 function action(){
